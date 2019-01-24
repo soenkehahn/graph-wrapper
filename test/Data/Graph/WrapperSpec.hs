d90a05270d45694f239fd1f0f3860bd0c40faa63
Samuel Gélineau <gelisam+github@gmail.com>
2019-01-24 07:53:13 -0500

    style

--- test/Data/Graph/WrapperSpec.hs
+++ test/Data/Graph/WrapperSpec.hs
@@ -31,8 +31,8 @@ spec = do
 
     context "when the graphs are isomorphic" $ do
       it "returns False" $ do
-        let g1 = fromList [('1','a',['2']), ('2','b',[])]
-        let g2 = fromList [('2','a',['1']), ('1','b',[])]
+        let g1 = fromList [('1', 'a', ['2']), ('2', 'b', [])]
+        let g2 = fromList [('2', 'a', ['1']), ('1', 'b', [])]
         g1 == g2 `shouldBe` False
 
     context "when the vertices are listed in a different order" $ do
